c15db8acac28df260bab20df085a5b7f5d01778f
Milankumabura <milanbudha007.mgr@gmail.com>
2023-07-23 12:59:59 +0545

    Initial commit

---
+++ src/main/java/com/example/istheSiteUp/IstheSiteUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.istheSiteUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstheSiteUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstheSiteUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/istheSiteUp/IstheSiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.istheSiteUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstheSiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
